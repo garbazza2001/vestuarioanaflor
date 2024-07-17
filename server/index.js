e4a7f1c19192b035140362f6896882b7f487cfb9
Lucas <lucasoom3@hotmail.com>
2024-07-17 14:25:28 -0300

    teste database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const mysql2 = require('mysql2'); 
 const app = express();
+const conexao =  mysql2.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'anaflor_banco',
+})
+
+conexao.connect(function(erro){
+    if(erro) throw erro;
+    console.log("ok");
+})
 
 app.get("/", (req,res)=>{
-    res.send("hello lucas")
+    const sqlInsert = "INSERT INTO produtos (nome_produto) VALUES ('tenis');"
+    db.query(sqlInsert, (err,result) => {
+        res.send("hello dsds ")
+    });
 });
 
 app.listen(3001, () => {
